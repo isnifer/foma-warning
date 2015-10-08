229b593696c9371abcea024dadc9c3c5b696cfcf
Anton Kuznetsov <isnifer@gmail.com>
2015-10-08 03:16:59 +0300

    Tests update

--- test/index.js
+++ test/index.js
@@ -1,4 +1,6 @@
+import './utils/jsdom';
 import test from 'tape';
+import { render, TestUtils } from './utils/render';
 import FomaWarning from '../index';
 
 test('export', assert => {
@@ -6,6 +8,62 @@ test('export', assert => {
         typeof FomaWarning === 'function',
         'Should be typeof function'
     );
+
+    assert.end();
+});
+
+test('items + hidden', assert => {
+    let props = {
+        visible: false,
+        items: [
+            {
+                fieldName: 'test',
+                name: 'test'
+            },
+            {
+                fieldName: 'test',
+                name: 'test'
+            }
+        ]
+    };
+
+    var Component = render(FomaWarning, props);
+
+    assert.equal(Component, null, 'Component hidden, null');
+
+    assert.end();
+});
+
+test('items + visible', assert => {
+    let props = {
+        visible: true,
+        items: [
+            {
+                fieldName: 'test',
+                name: 'test'
+            },
+            {
+                fieldName: 'test2',
+                name: 'test2'
+            }
+        ]
+    };
+
+    var Component = render(FomaWarning, props);
+
+    assert.equal(typeof Component.props.children, 'object', 'There is one field');
+
+    assert.end();
+});
+
+test('no items + visible', assert => {
+    let props = {
+        visible: true
+    };
+
+    var Component = render(FomaWarning, props);
+
+    assert.equal(Component, null, 'There is nothing to view, null');
 
     assert.end();
 });
